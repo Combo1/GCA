d236f8b4534d12ab5d81567b014ee3eba5b1cd35
Maximilian Geitner <maximilian.geitner@outlook.de>
2021-03-20 22:40:24 +0100

    tic tac toe screen

---
+++ lib/TicTacToeScreen.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+class TicTacToeScreen extends StatelessWidget{
+  static const routeName = '/games/tictactoe';
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child:
+        Scaffold(
+          body: new Text('TODO TicTacToe'),
+      ),
+    );
+  }
+
+}
