e7682a35be7519ca007d49b375c0673e938688a4
martin <mtjh98@gmx.de>
2021-04-05 21:20:10 +0200

    - added basic symbols for RPS

--- lib/RockPaperScissors.dart
+++ lib/RockPaperScissors.dart
@@ -8,12 +8,77 @@ class RPSStandard extends StatefulWidget {
 }
 
 class _RPSStandardState extends State<RPSStandard> {
+  void _handleButtonPressed(int sign) {
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Center(
+            child: Icon(
+          Icons.cut,
+          color: Colors.black,
+          size: 40.0,
+        )),
+        Row(
+          children: [
+            Button(sign: 0, onChanged: _handleButtonPressed),
+            Button(sign: 1, onChanged: _handleButtonPressed),
+            Button(sign: 2, onChanged: _handleButtonPressed),
+          ],
+        )
+      ],
+    );
   }
 }
 
+class Button extends StatelessWidget {
+  Button({Key key, this.sign: 0, @required this.onChanged}) : super(key: key);
+
+  final int sign;
+  final ValueChanged<int> onChanged;
+
+  void _handleTap() {
+    onChanged(sign);
+  }
+
+  Icon getIcon() {
+    switch (sign) {
+      case 0:
+        return Icon(
+          Icons.cut,
+          color: Colors.black,
+          size: 40.0,
+        );
+      case 1:
+        return Icon(
+          Icons.pan_tool,
+          color: Colors.black,
+          size: 40.0,
+        );
+      case 2:
+        return Icon(
+          Icons.cloud,
+          color: Colors.black,
+          size: 40.0,
+        );
+      default:
+        return Icon(
+          Icons.error,
+          color: Colors.black,
+          size: 40.0,
+        );
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(onTap: _handleTap, child: getIcon()),
+    );
+  }
+}
 
 class RPSStandardScreen extends StatelessWidget {
   static const routeName = '/games/rps/standard';
